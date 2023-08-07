1e0be61e2f68a6e9fdfefb83dd6cffe85aa58ba8
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-08-07 22:57:57 +0530

    - Setting up RV.
    - fetching messages from database handler.

--- app/src/main/java/com/droid/magicapp/message_logs/MessageLogsFragment.kt
+++ app/src/main/java/com/droid/magicapp/message_logs/MessageLogsFragment.kt
@@ -5,55 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.droid.magicapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.droid.magicapp.databinding.FragmentMessageLogsBinding
+import com.droid.magicapp.message_logs.adapters.MessageLogsAdapter
+import com.droid.magicapp.message_logs.adapters.OnSMSLogSelectedListenerCallback
+import com.droid.magicapp.message_logs.models.InboxMessage
+import com.droid.magicapp.message_logs.utils.MessageLogsDataBaseHandler
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MessageLogsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MessageLogsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMessageLogsBinding
+    private val messageLogsDataBaseHandler: MessageLogsDataBaseHandler =
+        MessageLogsDataBaseHandler()
+    private val phoneNumber: String = "7760237886"
+    private var messageLogsList: ArrayList<InboxMessage> = arrayListOf()
+    private lateinit var messageLogsAdapter: MessageLogsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message_logs, container, false)
+    ) = FragmentMessageLogsBinding.inflate(layoutInflater).also { binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        messageLogsDataBaseHandler.fetchMessagesForNumber(phoneNumber) { messageList ->
+            messageLogsList = messageList as ArrayList<InboxMessage>
+            messageLogsAdapter.clearAndAddData(messageLogsList)
+        }
+        setUpRv()
+    }
+
+    private fun setUpRv() {
+        messageLogsAdapter = MessageLogsAdapter(
+            messageLogsList,
+            requireContext(),
+            object : OnSMSLogSelectedListenerCallback {
+                override fun onSelected(position: Int) {
+
+                }
+
+            }
+        )
+        binding.rvSMSLogs.apply {
+            adapter = messageLogsAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MessageLogsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        val TAG2: String? = MessageLogsFragment::class.java.canonicalName
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             MessageLogsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
