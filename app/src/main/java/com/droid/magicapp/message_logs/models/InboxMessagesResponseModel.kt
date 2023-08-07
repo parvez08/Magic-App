ae0f6c61afe00e109b4a6d5f11be507e2d07a03a
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-08-07 22:56:07 +0530

    - Added null check.

--- app/src/main/java/com/droid/magicapp/message_logs/models/InboxMessagesResponseModel.kt
+++ app/src/main/java/com/droid/magicapp/message_logs/models/InboxMessagesResponseModel.kt
@@ -1,11 +1,7 @@
 package com.droid.magicapp.message_logs.models
 
-data class InboxMessagesResponse(
-    val inboxMessages: List<InboxMessage>
-)
-
 data class InboxMessage(
-    val address: String,
-    val body: String,
-    val date: Long
+    val address: String?,
+    val body: String?,
+    val date: Long?
 )
