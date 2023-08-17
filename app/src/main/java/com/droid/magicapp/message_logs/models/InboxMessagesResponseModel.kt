fe01562c8fd8bccca88abd3c96f66327eab6acbe
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-08-17 10:13:26 +0530

    - Modified Inbox Message model class and addded extra two params i.e, id and isSent.

--- app/src/main/java/com/droid/magicapp/message_logs/models/InboxMessagesResponseModel.kt
+++ app/src/main/java/com/droid/magicapp/message_logs/models/InboxMessagesResponseModel.kt
@@ -1,7 +1,13 @@
 package com.droid.magicapp.message_logs.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class InboxMessage(
+    val id:String?,
     val address: String?,
     val body: String?,
-    val date: Long?
-)
+    val date: Long?,
+    val isSent: Boolean?
+) : Parcelable
